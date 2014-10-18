b14d48117bff066e58e1f0df1fa5308ed6a8a4b9
Systwo <starfruitinc@gmail.com>
2014-10-17 21:23:26 -0400

    Add game module into main server code to test

--- server/config.py
+++ server/config.py
@@ -1,4 +1,10 @@
 import os.path
 
+
+#Careful with these, these are actually one level up from this folder
+#Maybe move these to an actual config (eg. ini, json) file?
 STATIC_CONTENT_PATH = os.path.join("client")
+
+unitListFile = os.path.join("shared", "UnitList.json")
+territoryListFile = os.path.join("shared", "TerritoryList.json")
 
--- server/server.py
+++ server/server.py
@@ -6,9 +6,9 @@ import os.path
 import config
 import utils
 
+import game
 
 STATIC_CONTENT_PATH = config.STATIC_CONTENT_PATH
-
 
 
 
@@ -20,7 +20,28 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class BoardHandler(tornado.web.RequestHandler):
 	def get(self, boardid):
+		iceland = game.Country("Iceland")
+
+		t1 = game.Territory("Ontario", 10, iceland)
+		t2 = game.Territory("Quebec", 10, iceland)
+		t3 = game.Territory("Nova Scotia", 10, iceland)
+
+		t1.connections.append(t2)
+		t2.connections.append(t3)
+		t2.connections.append(t1)
+		t3.connections.append(t2)
+
+		fighter = game.Unit("fighter", iceland, t1)
+
+		board = game.Board([fighter], [iceland], config.unitListFile, config.territoryListFile)
 		self.write(boardid)
+		self.write(str(board.getPath(t1, t2, fighter)))
+
+	def post(self, boardid):
+		req = self.request
+		self.write("boardid:" + str(boardid))
+		self.write(str(req.body))
+		#self.write(self.get_argument("key"))
 
 
 
@@ -28,7 +49,7 @@ class Server:
 	def __init__(self):
 		app = tornado.web.Application([
 			(r"/", IndexHandler),
-			(r"/board/([0-9]+)", BoardHandler), 
+			(r"/boards/([0-9]+)", BoardHandler), 
 			(r"/static/(.*)", utils.NoCacheStaticFileHandler, {"path": os.path.join(STATIC_CONTENT_PATH)}) #This is not a great way of doing this TODO: Change this to be more intuative
 		])
 
