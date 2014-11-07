1cb6b09dc47d1e6924c960d2d6f94b4ce374acd0
Systwo <starfruitinc@gmail.com>
2014-11-07 02:17:05 -0500

    Changed how boards are stored

--- server/server.py
+++ server/server.py
@@ -38,8 +38,8 @@ class BoardHandler(tornado.web.RequestHandler):
 class BoardsHandler(tornado.web.RequestHandler):
     #TODO: Consider spliting this class to handle the different scenarios
     actions = utils.Enum(["ALL", "NEW", "ID"])
-    # boards = []
-    boards = [game.Board("default"), game.Board("uw")]
+    boards = {} #We use a map here so we can easily delete boards
+    nextBoardId = 0
 
     def initialize(self, config, action):
         self.config = config
@@ -51,34 +51,65 @@ class BoardsHandler(tornado.web.RequestHandler):
             self.write("All")
         elif self.action == self.actions.NEW:
             #Make a new board
-            newBoard = game.Board("default")
-            print(newBoard.id)
-            self.boards.append(newBoard)
-            # self.redirect(r"/boards/" + str(random.randint(0, 100)))
+
+            #Grab settings from request
+            #settings = json.loads(self.request.body)
+
+            #TODO: Configure map (module), number of players here
+			
+
+            #Create and add the board to the working list of boards
+            BoardsHandler.boards[BoardsHandler.nextBoardId] = game.Board("default")
+            #Prepare the next boardId to use
+            BoardsHandler.nextBoardId += 1
+			
+            #Tell the client the id of the newly created board
+            self.write(json.dumps({"boardId": len(self.boards) - 1}))
         elif self.action == self.actions.ID:
             #Return info about board with id boardId
+
             board = self.getBoard(params["boardId"])
+            if not board:
+                self.set_status(404)
+                self.write("Board not found")
+                return
+
             # Return the board info as json
             boardInfo = board.toDict()
             boardInfo["imagePath"] = os.path.join(self.config.MODS_PATH, boardInfo["moduleName"], boardInfo["imageName"])
             self.write(json.dumps(boardInfo))
 
+
     def post(self, **params):
         if self.action == self.actions.ID:
-            req = self.request
-            self.write("boardid:" + str(params["boardId"]))
-            self.write(str(req.body))
+            if getBoard(params["boardId"]): #Make sure this is a valid board
+                #Make sure there is a body
+                if len(req.body) == 0:
+                    self.set_status(400)
+                    return
+
+                #Make sure there is a valid body
+                try:
+                    json.loads(req.body)
+                except ValueError:
+                    self.set_status(400)
+                    return
+
+                req = json.loads(req.body)
+				
+
         else:
             self.set_status(405)
             self.write("Method Not Allowed")
         return
 
+
     def getBoard(self, boardId):
-        for b in self.boards:
-            print("id " + b.id)
-            print(boardId)
-            if b.id == boardId:
-                return b
+        #We use ints for the id, so we force the boardId to be an int
+        normalizedBoardId = int(boardId)
+        if normalizedBoardId in BoardsHandler.boards:
+            return BoardsHandler.boards[normalizedBoardId]
+        return None
 
 
 
@@ -89,7 +120,7 @@ class Server:
         self.app = tornado.web.Application([
             (r"/", IndexHandler, dict(html_path=html_path)),
 
-			#Map editor
+			#Map editor (Consider moving into a sub-list in MapEditorHandler)
             (r"/mapEditor", MapEditorHandler, dict(config=config, action=MapEditorHandler.actions.PAGE, html_path=html_path)),
             (r"/modules", MapEditorHandler, dict(config=config, action=MapEditorHandler.actions.MODULES)),
             (r"/modules/create/?", MapEditorHandler, dict(config=config, action=MapEditorHandler.actions.CREATE)),
