79ac8b4cf2ca7641c6f62f05bbba88e7c08be262
Systwo <starfruitinc@gmail.com>
2015-01-11 19:37:22 -0500

    Add more meta info to the Game class

--- server/Game.py
+++ server/Game.py
@@ -4,29 +4,43 @@ class Game:
     def __init__(self, gameName, numPlayers, moduleName, password=""):
         #We are okay to have the password as a default arg, as strings are immutable
         self.name = gameName
-        self.numPlayers = numPlayers
-        self.creatorId = creatorId
+        self.currPlayers = 0
+        self.maxPlayers = numPlayers
+        self.creatorId = 0
         self.moduleName = moduleName
         self.password = password
         self.started = False
 
 
         
-        self.board = GameBoard.Board(moduleName)
+        #self.board = GameBoard.Board(moduleName)
 
         #This could be a array of tuples if we need it to be
         self.players = {} #Contains (country, userId) pairs
 
     """Attempts to add a player to a country. Returns True on success"""
     def addPlayer(self, userId, country):
+        if (self.currPlayers > self.maxPlayers):
+            return False
+        
+
+        if (country not in self.players):
+            self.players[country] = None
+
         if (self.players[country]):
             return False
 
         self.players[country] = userId
+        self.currPlayers += 1
         return True
 
     def removePlayer(self, country):
+        self.currPlayers -= 1
         self.players[country] = None
 
     def listPlayers(self):
         pass
+
+    def loadCountries(self, countries):
+        pass
+
