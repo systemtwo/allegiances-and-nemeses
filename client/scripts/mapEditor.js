1b164e57c10c09d775abd3f4dc7a1be73574fc74
dschwarz <dan-schwarz@hotmail.com>
2015-04-21 00:07:27 -0700

    Added path editing behavior
    Pass a set of nodes into the svg map. When dragged, they update the territory path.

--- client/scripts/mapEditor.js
+++ client/scripts/mapEditor.js
@@ -43,6 +43,7 @@ function (nj, _svg, _, msg, TerritoryEditorView) {
     }
 
     function initialize (moduleInfo) {
+        var nodes;
         _.each(["territories", "countries", "connections", "moduleName"], function (parameter) {
             if (!moduleInfo[parameter]) {
                 msg.log("Missing parameter: " + parameter)
@@ -62,13 +63,18 @@ function (nj, _svg, _, msg, TerritoryEditorView) {
             return [first, second];
         });
 
+        nodes = createNodes(mapData.territories);
+
         territoryEditorView.update({
             countryList: mapData.countries
         });
         territoryEditorView.render();
 
         svgMap = new _svg.Map(mapData, ".map-holder");
-        svgMap.update({showConnections: true});
+        svgMap.update({
+            showConnections: true,
+            nodes: nodes
+        });
         svgMap.drawMap();
         svgMap.on("click:territory", function (territoryName) {
             var territory = _.findWhere(mapData.territories, {name: territoryName});
@@ -80,6 +86,67 @@ function (nj, _svg, _, msg, TerritoryEditorView) {
 
         bindButtons();
         svgMap.setSelectableTerritories(mapData.territories);
+    }
+
+    var Node = function (point) {
+        this.point = {
+            x: point[0],
+            y: point[1]
+        };
+        this.references = [point];
+        return this;
+    };
+    Node.prototype.getX = function () {
+        return this.point.x;
+    };
+    Node.prototype.getY = function () {
+        return this.point.y;
+    };
+    Node.prototype.updatePoint = function (x, y) {
+        var node = this;
+        if (_.isFinite(x)) {
+            this.point.x = x;
+        }
+        if (_.isFinite(y)) {
+            this.point.y = y;
+        }
+
+        _.each(node.references, function (linkedPoint) {
+            linkedPoint[0] = node.point.x;
+            linkedPoint[1] = node.point.y;
+        })
+    };
+    Node.prototype.addReference = function (newPoint) {
+        var node = this;
+        if (!node.atPoint(newPoint)) {
+            console.error("Trying to add non-corresponding point");
+            return;
+        }
+
+        this.references.push(newPoint);
+    };
+    Node.prototype.atPoint = function(point) {
+        var node = this;
+        return node.point.x == point[0] && node.point.y == point[1];
+    };
+
+    function createNodes (territories) {
+        var nodes = [], path;
+        _.each(territories, function (territory) {
+            path = territory.displayInfo.path;
+            if (!_.isArray(path)) throw "No path provided for territory " + territory.displayName;
+            _.each(path, function (point) {
+                var correspondingNode = _.find(nodes, function (node) {
+                    return node.atPoint(point);
+                });
+                if (correspondingNode) {
+                    correspondingNode.addReference(point);
+                } else {
+                    nodes.push(new Node(point));
+                }
+            })
+        });
+        return nodes;
     }
 
     function territoriesEqual (territory, other) {
--- client/scripts/svgMap.js
+++ client/scripts/svgMap.js
@@ -1,16 +1,17 @@
 define(["lib/d3", "underscore", "backbone"],
-function(d3, _, backbone) {
-    var Map = function(mapInfo, appendTo) {
+function (d3, _, backbone) {
+    var Map = function (mapInfo, appendTo) {
         var map = this;
         var zoom = d3.behavior.zoom()
             .scaleExtent([0.1, 10])
-            .on("zoom", function zoomed() {
+            .on("zoom", function zoomed () {
               map.container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
             });
         this.showConnections = false;
         this.connections = [];
         this.territories = [];
         this.selectableTerritories = [];
+        this.nodes = [];
         this.parse(mapInfo);
 
         this.mapElement = d3.select(appendTo)
@@ -23,12 +24,19 @@ function(d3, _, backbone) {
         this.container = this.mapElement.append("g")
             .classed("map-element-container", true);
 
+        this.territoryContainer = this.container.append("g")
+            .classed("connections-container", true);
+        this.connectionsContainer = this.container.append("g")
+            .classed("connections-container", true);
+        this.nodesContainer = this.container.append("g")
+            .classed("nodes-container", true);
+
         return this;
     };
     var proto = Map.prototype;
     _.extend(proto, backbone.Events);
 
-    proto.parse = function(mapInfo) {
+    proto.parse = function (mapInfo) {
         var map = this;
         if (_.isArray(mapInfo.territories)) {
             this.territories = mapInfo.territories;
@@ -36,7 +44,7 @@ function(d3, _, backbone) {
 
         if (_.isArray(mapInfo.connections)) {
             this.connections = _.map(mapInfo.connections, function (pair) {
-                return _.map(pair, function(maybeTerritory) {
+                return _.map(pair, function (maybeTerritory) {
                     if (_.contains(map.territories, maybeTerritory)) {
                         return maybeTerritory;
                     } else {
@@ -50,30 +58,36 @@ function(d3, _, backbone) {
                 })
             });
         }
+        
+        if (_.isArray(mapInfo.nodes)) {
+            this.nodes = mapInfo.nodes;
+        }
+        
         // update if passed in, otherwise keep same value or default to false
         if (!_.isUndefined(mapInfo.showConnections)) {
             this.showConnections = mapInfo.showConnections;
         }
     };
 
-    proto.setSelectableTerritories = function(territories) {
+    proto.setSelectableTerritories = function (territories) {
         this.selectableTerritories = territories;
         this.drawMap();
     };
 
-    proto.update = function(mapInfo) {
+    proto.update = function (mapInfo) {
         this.parse(mapInfo);
         this.drawMap();
     };
 
-    proto.drawMap = function() {
+    proto.drawMap = function () {
         this.drawTerritories();
-        this.drawConnections(this.connections);
+        this.drawConnections();
+        this.drawNodes();
     };
 
     proto.drawTerritories = function () {
         var map = this;
-        var territoryGroups = this.container.selectAll(".territory-group")
+        var territoryGroups = this.territoryContainer.selectAll(".territory-group")
             .data(this.territories);
 
         territoryGroups.exit().remove();
@@ -94,29 +108,29 @@ function(d3, _, backbone) {
 
 
         territoryGroups
-            .classed("selectable", function(data){
+            .classed("selectable", function (data){
                 return _.contains(map.selectableTerritories, data)
             });
 
         territoryGroups.select(".territory")
-            .attr("d", function(data) {
-                var corePath = _.map(data.displayInfo.path, function(points) {
+            .attr("d", function (data) {
+                var corePath = _.map(data.displayInfo.path, function (points) {
                     return points.join(",");
                 }).join(" L");
 
                 return "M" + corePath + " z";
             });
         territoryGroups.select(".unit-selector")
-            .attr("cx", function(data) { return data.displayInfo.circle.x; })
-            .attr("cy", function(data) { return data.displayInfo.circle.y; });
+            .attr("cx", function (data) { return data.displayInfo.circle.x; })
+            .attr("cy", function (data) { return data.displayInfo.circle.y; });
         territoryGroups.select(".territory-name")
-            .text(function(d) {return d.displayName})
-            .attr("x", function(data) { return data.displayInfo.name.x; })
-            .attr("y", function(data) { return data.displayInfo.name.y; })
+            .text(function (d) {return d.displayName})
+            .attr("x", function (data) { return data.displayInfo.name.x; })
+            .attr("y", function (data) { return data.displayInfo.name.y; })
             .on("click", map.onTerritoryClick.bind(map));
     };
-    proto.drawConnections = function(connections) {
-        var connectionData = _.map(connections, function(pair) {
+    proto.drawConnections = function () {
+        var connectionData = _.map(this.connections, function (pair) {
             return {
                 x1: pair[1].displayInfo.name.x,
                 x2: pair[0].displayInfo.name.x,
@@ -124,7 +138,7 @@ function(d3, _, backbone) {
                 y2: pair[0].displayInfo.name.y
             }
         });
-        var lines = this.container.selectAll(".connection")
+        var lines = this.connectionsContainer.selectAll(".connection")
             .data(connectionData);
 
         lines.enter().append("line")
@@ -133,26 +147,69 @@ function(d3, _, backbone) {
 
         lines
             .style("display", this.showConnections ? "" : "none")
-            .attr("x1", function(d){return d.x1})
-            .attr("x2", function(d){return d.x2})
-            .attr("y1", function(d){return d.y1})
-            .attr("y2", function(d){return d.y2});
-
-
+            .attr("x1", function (d){return d.x1})
+            .attr("x2", function (d){return d.x2})
+            .attr("y1", function (d){return d.y1})
+            .attr("y2", function (d){return d.y2});
+        
         lines.exit().remove();
     };
+    
+    proto.drawNodes = function () {
+        var map = this;
+        var drag = d3.behavior.drag()
+            .origin(function(node) {
+                return {
+                    x: node.getX(),
+                    y: node.getY()
+                };
+            })
+            .on("dragstart", dragstarted)
+            .on("drag", dragged)
+            .on("dragend", dragended);
+        var nodeElements = this.nodesContainer.selectAll(".node")
+            .data(this.nodes);
 
-    proto.onMapClick = function() {
+        nodeElements.enter().append("circle")
+            .classed("node", true)
+            .attr("r", 5)
+            .call(drag);
+
+        nodeElements
+            .attr("cx", function (d) { return d.getX(); })
+            .attr("cy", function (d) { return d.getY(); });
+
+        nodeElements.exit().remove();
+
+        function dragstarted() {
+            d3.event.sourceEvent.stopPropagation();
+            d3.select(this).classed("dragging", true);
+        }
+
+        function dragged(node) {
+            node.updatePoint(d3.event.x, d3.event.y);
+            d3.select(this)
+                .attr("cx", node.getX())
+                .attr("cy", node.getY());
+            map.drawTerritories();
+        }
+
+        function dragended() {
+            d3.select(this).classed("dragging", false);
+        }
+    };
+
+    proto.onMapClick = function () {
         if (d3.event.defaultPrevented) return; // click suppressed
         this.trigger("click");
     };
 
-    proto.onTerritoryClick =  function(data){
+    proto.onTerritoryClick =  function (data){
         if (d3.event.defaultPrevented) return; // click suppressed
         this.trigger("click:territory", data.name);
     };
 
-    proto.onCircleClick = function(data){
+    proto.onCircleClick = function (data){
         if (d3.event.defaultPrevented) return; // click suppressed
         this.trigger("click:circle", data.name);
     };
